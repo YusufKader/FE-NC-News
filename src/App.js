425ffc09f544915391e3ab1b5a8c11121e070616
YusufKader <yusufkader7@gmail.com>
2022-09-27 14:37:24 +0100

    updated Ticket 4 NC News

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import Header from './Components/Header';
-import { Route, Routes } from "react-router-dom"
+import { Link, Route, Routes } from "react-router-dom"
 import Articles from './Components/Articles';
+import { Home } from './Components/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/articles">Articles</Link></li>
+        </ul>
+      </nav>
       <Routes>
-        <Route path="articles" element={<Articles />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/articles" element={<Articles />}></Route>
       </Routes>
     </div>
   );
